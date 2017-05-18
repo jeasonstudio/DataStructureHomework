721bba16b24b0ad023962a97e4d18748cc4211b3
赵吉彤 <zjtong3576@sina.com>
2017-05-18 11:11:23 +0800

    add

--- MATLAB/U5/test.m
+++ MATLAB/U5/test.m
@@ -4,5 +4,13 @@ function output = test()
 % Syntax: output = test()
 %
 % Long description
+    x = 0:6;
+    y = cos(x);
+    xi = 0:.25:6;
+    yi1 = interp1(x,y,xi,'*nearest');
+    yi2 = interp1(x,y,xi,'*linear');
+    yi3 = interp1(x,y,xi,'*spline');
+    yi4 = interp1(x,y,xi,'*cubic');
+    plot(x, y, 'ro', xi, yi1, '--', xi, yi2, '-', xi, yi3, 'k.-', xi, yi4, 'm:')
     
 end
