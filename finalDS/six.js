ade776eb4b5ed60227d644423f2a363f2a913455
赵吉彤 <zjtong3576@sina.com>
2017-05-22 00:54:47 +0800

    quick sort

--- finalDS/six.js
+++ finalDS/six.js
@@ -1,6 +1,18 @@
-let resultObj = {},
-    eng = require('./eng.js').match(/[a-z]+[\-\']?[a-z]*/ig);
-eng.map(r => resultObj[r] ? resultObj[r]++ : resultObj[r] = 1)
+let resultObj = {}
+require('./eng.js').match(/[a-z]+[\-\']?[a-z]*/ig).map(r => resultObj[r] ? resultObj[r]++ : resultObj[r] = 1)
 
 // console.log('module.exports = ', resultObj)
-// console.log(resultObj)
+console.log(...quickSort(Object.keys(resultObj)))
+// console.log(quickSort(Object.keys(resultObj)).length)
+
+function quickSort(arr) {
+    if (arr.length <= 1) return arr
+    let pivot = parseInt(arr.length / 2),
+        left = [],
+        equal = [],
+        right = [];
+    arr.map(key => {
+        resultObj[key] > resultObj[arr[pivot]] ? left.push(key) : (resultObj[key] == resultObj[arr[pivot]] ? equal.push(key) : right.push(key))
+    })
+    return quickSort(left).concat(equal).concat(quickSort(right))
+}
