9328edbeafb487179721dd5ae648ab8ad262a59a
赵吉彤 <zjtong3576@sina.com>
2017-05-22 01:09:08 +0800

    finish six

--- finalDS/six.js
+++ finalDS/six.js
@@ -1,18 +1,14 @@
-let resultObj = {}
-require('./eng.js').match(/[a-z]+[\-\']?[a-z]*/ig).map(r => resultObj[r] ? resultObj[r]++ : resultObj[r] = 1)
-
-// console.log('module.exports = ', resultObj)
-console.log(...quickSort(Object.keys(resultObj)))
-// console.log(quickSort(Object.keys(resultObj)).length)
-
-function quickSort(arr) {
-    if (arr.length <= 1) return arr
-    let pivot = parseInt(arr.length / 2),
-        left = [],
-        equal = [],
-        right = [];
-    arr.map(key => {
-        resultObj[key] > resultObj[arr[pivot]] ? left.push(key) : (resultObj[key] == resultObj[arr[pivot]] ? equal.push(key) : right.push(key))
-    })
-    return quickSort(left).concat(equal).concat(quickSort(right))
-}
+(function () {
+    let resultObj = new Object()
+    require('./eng.js').match(/[a-z]+[\-\']?[a-z]*/ig).map(r => resultObj[r] ? resultObj[r]++ : resultObj[r] = 1)
+    let resu = quickSort(Object.keys(resultObj))
+    new Array(10).fill(0).map((a, m) => console.log('第' + (m + 1) + '个 : ', resu[m], resultObj[resu[m]]))
+    function quickSort(arr) {
+        if (arr.length <= 1) return arr
+        let [pivot, left, equal, right] = [parseInt(arr.length / 2), [], [], []]
+        arr.map(key => {
+            resultObj[key] > resultObj[arr[pivot]] ? left.push(key) : (resultObj[key] == resultObj[arr[pivot]] ? equal.push(key) : right.push(key))
+        })
+        return quickSort(left).concat(equal).concat(quickSort(right))
+    }
+})()
