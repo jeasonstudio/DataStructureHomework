4905752da5e5f02bcaf8703a21adf8a8cd11243b
赵吉彤 <zjtong3576@sina.com>
2017-06-07 23:23:25 +0800

    update

--- MATLAB/exam/test.m
+++ MATLAB/exam/test.m
@@ -1,4 +1,22 @@
 clear
 format long
 
-dsolve('Dy=x*sin(x)/cos(y)','x')
+function y = mu(X)
+    n=length(X);
+    s=sum(X);
+    y=s/n;
+end
+
+function y = sigma2(X)
+    Y=X-mu(X);
+    Y2=Y.*Y;
+    n=length(X);
+    s=sum(Y2);
+    y=s/n;
+end
+
+load a.txt
+mu=mu(a)
+sig=sigma2(a)
+[mu,sigma,muci,sigmaci]=normfit(a,0.95)
+[mu,sigma,muci,sigmaci]=normfit(a,0.99)
