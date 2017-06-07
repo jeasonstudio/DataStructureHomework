3a8eb84325d031cbb726d6a8ae341f913a11d858
赵吉彤 <zjtong3576@sina.com>
2017-06-07 23:44:51 +0800

    ok

---
+++ MATLAB/exam/ly7zuo2.m
@@ -0,0 +1,7 @@
+clc
+clear
+clf
+load A.txt;
+A
+[N,X]=hist(A(:),7)
+hist(A(:),7)
--- MATLAB/exam/test.m
+++ MATLAB/exam/test.m
@@ -1,22 +1,8 @@
 clear
 format long
 
-function y = mu(X)
-    n=length(X);
-    s=sum(X);
-    y=s/n;
-end
+load b.txt
+figure(1)
 
-function y = sigma2(X)
-    Y=X-mu(X);
-    Y2=Y.*Y;
-    n=length(X);
-    s=sum(Y2);
-    y=s/n;
-end
-
-load a.txt
-mu=mu(a)
-sig=sigma2(a)
-[mu,sigma,muci,sigmaci]=normfit(a,0.95)
-[mu,sigma,muci,sigmaci]=normfit(a,0.99)
+[N,X]=hist(b(:),7)
+hist(b(:),7)
