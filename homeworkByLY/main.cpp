1a38bfb3beac7f9c6cbc33aa000926418e9bc511
Jeason <zhaojitong@xiaomi.com>
2017-11-14 22:44:50 +0800

    finish

--- homeworkByLY/main.cpp
+++ homeworkByLY/main.cpp
@@ -6,34 +6,50 @@
 #include<fstream>
 using namespace std;
   
-void process (string filename, string theWord) {
-  map<string,int> word_count;
+int process (string filename, string theWord) {
+  // map<string,int> word_count;
   ifstream infile;
   // 1024 is a joke
   char line[1024];
-   
+
   infile.open(filename.c_str(),ios::in);
+  int result = 0;
+  string::size_type position;
 
   //read line from file and process
   while (infile.getline(line, 1024, '\n')) {
     string word;
     istringstream str(line);
     while (str >> word) {
-      ++word_count[word];
+      position = word.find(theWord);
+      if (position != word.npos) result++;
     }
   }
-
-  for (map<string, int>::iterator it = word_count.begin(); it != word_count.end(); ++it )
-    if (it->first == theWord)
-      cout << filename <<" "<<it->second<<endl;
+  return result;
 }
 
 int main (void) {
 
-  string files[4] = {"Andy.txt", "Novak.txt", "Rafa.txt", "Roger.txt"};
+  // 128 means file `table_of_content.txt` should be less than 128 lines.
+  string files[128];
+  char line[1024];
   string action;
+  ifstream infile;
+  string tableOfContent = "table_of_content.txt";
+  infile.open(tableOfContent.c_str(),ios::in);
+  for (int j = 0; infile.getline(line, 1024, '\n'); j++) {
+    string targetFile;
+    istringstream str(line);
+    str >> targetFile;
+    files[j] = targetFile;
+  }
 
-  cout << "4 students' text files are read." << endl;
+  int fileNum = 0;
+  for (int num = 0; num < 128; num++) {
+    if (!files[num].empty()) fileNum++;
+  }
+
+  cout << fileNum << " students' text files are read." << endl;
   cout << "------\n" << "Options:\n" <<"1. Keyword Search\n2. Exit" << endl;
   
   cin >> action;
@@ -41,10 +57,13 @@ int main (void) {
     cout << "Your choice:" << endl;
     string choiceWord;
     cin >> choiceWord;
-    for (int i = 0; i < 4; i++) {
-      process(files[i], choiceWord);
+    for (int i = 0; i < 128; i++) {
+      if (!files[i].empty()) {
+        // Print result here.
+        cout << files[i] << " " << process(files[i], choiceWord) << endl;
+      } else continue;
     }
-    cout << "------\n" << "Options:\n" <<"1. Keyword Search\n2.Exit"<< endl;
+    cout << "------\n" << "Options:\n" <<"1. Keyword Search\n2. Exit"<< endl;
     cin >> action;
   }
 }
